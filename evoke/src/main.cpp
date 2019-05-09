96c427afa4ea325f347976e69fc34b9439fd187a
JulienGrv <grave.jul@gmail.com>
2019-05-09 15:16:04 +0200

    use boost/program_options for the command line interface

--- evoke/src/main.cpp
+++ evoke/src/main.cpp
@@ -33,46 +33,26 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> v)
     return os;
 }
 
-void parseArgs(std::vector<std::string> args, std::map<std::string, std::string &> argmap, std::map<std::string, bool &> toggles)
-{
-    for(size_t index = 0; index < args.size();)
-    {
-        auto toggle_it = toggles.find(args[index]);
-        if(toggle_it != toggles.end())
-        {
-            toggle_it->second = true;
-            ++index;
-        }
-        else
-        {
-            auto it = argmap.find(args[index]);
-            if(it != argmap.end() && index + 1 != args.size())
-            {
-                it->second = args[index + 1];
-                index += 2;
-            }
-            else
-            {
-                std::cout << "Invalid argument: " << args[index] << "\n";
-                ++index;
-            }
-        }
-    }
-}
-
 int main(int argc, const char **argv)
 {
-    std::string toolset_name;
-
+    // Init arguments
+    bool help = false;
+    bool verbose = false;
+    std::string root_path = filesystem::current_path().generic_string();
+    unsigned int job_count = std::max(4u, std::thread::hardware_concurrency());
 #if defined(_WIN32)
-    toolset_name = "windows";
+    std::string toolset_name = "windows";
 #else
-    toolset_name = "gcc";
+    std::string toolset_name = "gcc";
 #endif
+    std::string reporter_name = "guess";
+    bool compilation_database = false;
+    bool cmakelists = false;
+    bool unity_build = false;
 
     // Init arguments parser
     po::options_description desc("Options");
-    desc.add_options()("help", "produce help message")("toolset-name", po::value<std::string>(&toolset_name), "specify toolset name (default: )");
+    desc.add_options()("help,h", po::bool_switch(&help), "print this message")("verbose", po::bool_switch(&verbose), "enable verbose output")("root", po::value(&root_path), "root directory path of the project")("jobs,j", po::value(&job_count), "build in parallel using N jobs")("toolset,t", po::value(&toolset_name), "toolset name")("reporter,r", po::value(&reporter_name), "reporter name")("cp", po::bool_switch(&compilation_database), "generate compile_commands.json")("cm", po::bool_switch(&cmakelists), "generate CMakelists.txt")("unity,u", po::bool_switch(&unity_build), "create commands for Unity");
 
     // Parse arguments
     po::variables_map vm;
@@ -80,32 +60,16 @@ int main(int argc, const char **argv)
     po::notify(vm);
 
     // Handle arguments
-    if(vm.count("help"))
+    if(help)
     {
         std::cout << desc << "\n";
-        return 0;
     }
-
-    if(vm.count("toolset-name"))
+    else
     {
-        std::cout << "Toolset: "
-                  << vm["toolset-name"].as<std::string>() << ".\n";
-    }
-
-    std::cout << "Building for " << toolset_name << std::endl;
-
-    std::string rootpath = filesystem::current_path().generic_string();
-    std::string jobcount = std::to_string(std::max(4u, std::thread::hardware_concurrency()));
-    std::string reporterName = "guess";
-    bool compilation_database = false;
-    bool cmakelists = false;
-    bool verbose = false;
-    bool unitybuild = false;
-    parseArgs(std::vector<std::string>(argv + 1, argv + argc), {{"-t", toolset_name}, {"--root", rootpath}, {"-j", jobcount}, {"-r", reporterName}}, {{"-cp", compilation_database}, {"-v", verbose}, {"-cm", cmakelists}, {"-u", unitybuild}});
-    Project op(rootpath);
-    if(!op.unknownHeaders.empty())
-    {
-        /*
+        Project op(root_path);
+        if(!op.unknownHeaders.empty())
+        {
+            /*
       // TODO: allow building without package fetching somehow
       std::string fetch = "accio fetch";
       std::vector<std::string> hdrsToFetch(op.unknownHeaders.begin(), op.unknownHeaders.end());
@@ -113,44 +77,56 @@ int main(int argc, const char **argv)
       system(fetch.c_str());
       op.Reload();
     */
-    }
-    for(auto &u : op.unknownHeaders)
-    {
-        std::cerr << "Unknown header: " << u << "\n";
-    }
-    std::unique_ptr<Toolset> toolset = GetToolsetByName(toolset_name);
-    if(unitybuild)
-    {
-        toolset->CreateCommandsForUnity(op);
-    }
-    else
-    {
-        toolset->CreateCommandsFor(op);
-    }
-    if(compilation_database)
-    {
-        std::ofstream os("compile_commands.json");
-        op.dumpJsonCompileDb(os);
-    }
-    if(cmakelists)
-    {
-        auto opts = toolset->ParseGeneralOptions(Configuration::Get().compileFlags);
-        op.dumpCMakeListsTxt(opts);
-    }
-    if(verbose)
-    {
-        std::cout << op;
-    }
-    std::unique_ptr<Reporter> reporter = Reporter::Get(reporterName);
-    Executor ex(std::stoul(jobcount), *reporter);
-    for(auto &comp : op.components)
-    {
-        for(auto &c : comp.second.commands)
-        {
-            if(c->state == PendingCommand::ToBeRun)
-                ex.Run(c);
         }
+        for(auto &u : op.unknownHeaders)
+        {
+            std::cerr << "Unknown header: " << u << "\n";
+        }
+
+        std::cout << "Building for " << toolset_name << "\n";
+        std::unique_ptr<Toolset> toolset = GetToolsetByName(toolset_name);
+
+        if(unity_build)
+        {
+            toolset->CreateCommandsForUnity(op);
+        }
+        else
+        {
+            toolset->CreateCommandsFor(op);
+        }
+
+        if(compilation_database)
+        {
+            std::ofstream os("compile_commands.json");
+            op.dumpJsonCompileDb(os);
+        }
+
+        if(cmakelists)
+        {
+            auto opts = toolset->ParseGeneralOptions(Configuration::Get().compileFlags);
+            op.dumpCMakeListsTxt(opts);
+        }
+
+        if(verbose)
+        {
+            std::cout << op;
+        }
+
+        std::unique_ptr<Reporter> reporter = Reporter::Get(reporter_name);
+
+        Executor ex(job_count, *reporter);
+        for(auto &comp : op.components)
+        {
+            for(auto &c : comp.second.commands)
+            {
+                if(c->state == PendingCommand::ToBeRun)
+                    ex.Run(c);
+            }
+        }
+        ex.Start().get();
+
+        printf("\n\n");
     }
-    ex.Start().get();
-    printf("\n\n");
+
+    return 0;
 }
